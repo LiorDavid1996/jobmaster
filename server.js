1829124913d664cf7529044a132e2cd12815783c
LiorDavid1996 <liordavid356@gmail.com>
2023-07-24 18:42:04 +0300

    routs&middleare

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+require('dotenv').config();
+require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.send('job api')
+})
 const port = process.env.PORT || 5000;
 
-app.listen(port, () =>
-console.log(`Server is listening on port ${port}...`)
+const start = async () => {
+    try {
+        app.listen(port, () =>
+       console.log(`Server is listening on port ${port}...`)
 );
+    } catch (error) {
+        console.log(error);
+    }
+}
+start()
