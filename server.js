de7b4812bcda13559bc4053eeb93692aa0b63cef
LiorDavid1996 <liordavid356@gmail.com>
2023-07-25 01:16:58 +0300

    register-and-login

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
-
+const connectDB = require('./db/connect');
+app.use(express.json())
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-app.use(express.json())
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
 app.get('/',(req,res)=>{
     res.send('job api')
 })
 const port = process.env.PORT || 5000;
-
+app.use('/api/auth', authRouter);
+app.use('/api/jobs', jobsRouter);
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URI);
         app.listen(port, () =>
        console.log(`Server is listening on port ${port}...`)
 );
