481cd18f114e60d0b0fe5dda3266382924f46889
LiorDavid1996 <liordavid356@gmail.com>
2023-07-26 01:25:54 +0300

    redux-toolkit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import   Error  from './pages/Error';
 const App = () => {
   return(
- <h1>jkdjjd</h1>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Error/>}/>
+
+    
+  </Routes>
+  </BrowserRouter>
   );
 };
 export default App;
