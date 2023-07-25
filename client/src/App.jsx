fa3f749b47a9eba4ef1b18a35c540a2ea51cd9d4
LiorDavid1996 <liordavid356@gmail.com>
2023-07-26 00:55:36 +0300

    errorPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,8 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import   Error  from './pages/Error';
 const App = () => {
-  return <h1>Jobify App</h1>;
+  return(
+ <h1>jkdjjd</h1>
+  );
 };
 export default App;
---
+++ client/src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
+
+const Error = () => {
+  return (
+    <h1>kkdk</h1>
+ 
+  );
+};
+export default Error;
