c15d4dd0f8cb78f8c4d9009375abbde2b69d7782
LiorDavid1996 <liordavid356@gmail.com>
2023-08-05 23:13:58 +0300

    test set up

---
+++ client/jest.config.cjs
@@ -0,0 +1,10 @@
+require = require('esm')(module);
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  // ... other Jest configuration options ...
+};
+
+
+  
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from './App';
+const React = require('react');
+const { render } = require('@testing-library/react');
+const { BrowserRouter, Route, Routes } = require('react-router-dom');
+const App = require('./App').default; // Use .default because App exports a default component
 
 test('renders home page', () => {
   const { getByText } = render(
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<div>Home Page</div>} />
-      </Routes>
-    </BrowserRouter>
+    React.createElement(BrowserRouter, null, React.createElement(Routes, null, React.createElement(Route, { path: '/', element: React.createElement('div', null, 'Home Page') })))
   );
   const homeElement = getByText('Home Page');
   expect(homeElement).toBeInTheDocument();
